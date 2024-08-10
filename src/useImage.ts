c2965d97ffac445c2ebac3b6d41728faebbc0599
eachann1024 <each1024@qq.com>
2024-08-11 01:03:32 +0800

    feat 1. 增加本地图片路径转 base64 2. 默认导出所有方法

--- src/useImage.ts
+++ src/useImage.ts
@@ -18,3 +18,31 @@ export function blobToFile(blob: Blob, fileName: string, mimeType: string): File
   const file = new File([blob], fileName, { type: mimeType })
   return file
 }
+
+/**
+ * 将本地图片 url 转成 base64
+ * @param path 静态路径
+ * @returns base64
+ */
+export async function toBase64(path: string): Promise<string> {
+  try {
+    const res = await fetch(path)
+    const blob = await res.blob()
+    const reader = new FileReader()
+    return new Promise<string>((resolve, reject) => {
+      reader.onloadend = () => resolve(reader.result as string)
+      reader.onerror = reject
+      reader.readAsDataURL(blob)
+    })
+  } catch (error) {
+    console.error(error)
+    throw new Error('图片转 base64 失败')
+  }
+}
+
+
+export default {
+  fileToBlob,
+  blobToFile,
+  toBase64
+}
